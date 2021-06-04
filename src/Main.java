7a1ecea2d5dc98fa93c057f82ac722866e0ae0df
evi19630494 <evinurmulia@gmail.com>
2021-06-04 05:34:12 -0700

    Project IDEA

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main (String[] args){
+
+        String namaDepan = "Evi";
+        String namaBelakang = "Normulia";
+        int usia = 20;
+        int targetTahunKuliah = 4;
+        double ipk = 3.89764512;
+        char nilaiAbjad = 'A';
+        boolean cantik = true;
+
+        System.out.println(namaDepan + namaBelakang);
+    }
+}
