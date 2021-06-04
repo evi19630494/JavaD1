9621984a1d140e6133b1e881d6c61949d4d58eb4
evi19630494 <evinurmulia@gmail.com>
2021-06-04 04:13:28 -0700

    Output dengan sout

---
+++ DataTypes.java
@@ -0,0 +1,20 @@
+public class DataTypes {
+  public static void main (String[] args){
+
+    String namaDepan = "Evi";
+    String namaBelakang = "Normulia";
+    int usia = 20;
+    int targetTahunKuliah = 4;
+    double ipk = 3.89764512;
+    char nilaiAbjad = 'A';
+    boolean cantik = true;
+
+    System.out.println("Nama Depan : " + namaDepan);
+    System.out.println("Nama Belakang : " + namaBelakang);
+    System.out.println("Usia : " + usia);
+    System.out.println("Target Kuliah : " + targetTahunKuliah + "tahun");
+    System.out.println("IPK : " + ipk);
+    System.out.println("Nilai PBO : " + nilaiAbjad);
+    System.out.println("Cantik : " + cantik);
+  }
+}
