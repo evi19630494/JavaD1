bbb2ac5f4be84ac57a84ff73b181ab207235467e
evi19630494 <evinurmulia@gmail.com>
2021-06-04 06:38:03 -0700

    Operasi Tipe String

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
+import java.util.Locale;
+
 public class Main {
     public static void main (String[] args){
 
-        char[] uniskaChar = {'U','N','I','S','K','A'};
-        String uniskaString = new String(uniskaChar);
         String namaDepan = "Evi";
         String namaBelakang = "Normulia";
         int usia = 20;
@@ -12,6 +12,26 @@ public class Main {
         boolean cantik = true;
 
         System.out.println(namaDepan + namaBelakang);
+
+        char[] uniskaChar = {'U','N','I','S','K','A'};
+        String uniskaString = new String(uniskaChar);
         System.out.println(uniskaString);
+
+        String namaLengkap = namaDepan + " " + namaBelakang;
+        String target, replacement, regex;
+        System.out.println(namaLengkap.length());
+        System.out.println(namaLengkap.indexOf("NIS"));
+        System.out.println(namaLengkap.substring(5));
+        System.out.println(namaLengkap.substring(5,12));
+        System.out.println(namaLengkap.replace(target = "Nor", replacement = "lia"));
+        System.out.println(namaLengkap.toUpperCase());
+        System.out.println(namaLengkap.toLowerCase());
+        System.out.println(namaLengkap.charAt(6));
+
+        String[] namaArray = namaLengkap.split(regex = " ");
+
+        for(String nama : namaArray){
+            System.out.println(nama);
+        }
     }
 }
